3dab971264a34ea90d476f49bfd32ef0b809e6b7
Aleksandr Savostin <asavostin1@gmail.com>
2022-07-27 09:02:18 +0500

    форма ркгистрации и авторизации

--- src/components/loginForm/index.js
+++
@@ -1,7 +0,0 @@
-import Handlebars from "handlebars";
-import tpl from "./tpl.hbs";
-import "./style.less";
-
-Handlebars.registerPartial('loginForm', tpl);
-
-export default title => tpl({title});
--- src/index.js
+++ src/index.js
@@ -7,38 +7,42 @@ import page500 from './pages/500';
 import pageAuthorization from './pages/authorization';
 import pageUserSettings from './pages/userSettings';
 import loginForm from './components/loginForm';
+import input from './components/input';
+import formAuthorization from './components/formAuthorization';
+import popup from './components/popup';
+import pageRegistration from './pages/registration';
+import formRegistration from './components/formRegistration';
 
 const configurations = {
-	'index': {
-		pageIndex: 'Навигация по страницам',
-		ItemPage404: 'Страница 404',
-		itemPage500: 'Страница 500',
-		itemPageAuthorization: 'Страница авторизация',
-		itemPageUserSettings: 'Страница настройки пользователя',
-	},
+	'index': pageIndex('Навигация по страницам'),
 	'link-1': page404('404'),
 	'link-2': page500('500'),
 	'link-3': pageAuthorization({
+		formAuthorization: true,
 		title: 'Вход',
-		btn: button('authprize', 'Авторизоваться'),
-		btn2: button('authprizeEmpty', 'Нет аккаунта?')
 	}),
-	'link-4': pageUserSettings('Страница настройки пользователя')
+	'link-4': pageRegistration({
+		title: 'Регистрация',
+		formRegistration: true,
+	}),
+	'link-5': pageUserSettings('Страница настройки пользователя')
 };
 
+const renderApp = (key) => {
+	const app = document.getElementById('app');
+	const template = document.createElement('template');
+
+	document.getElementById('app').innerHTML = '';
+	template.innerHTML = configurations[key];
+	app.appendChild(template.content);
+}
+
 window.onload = () => {
-	document.getElementById('app').innerHTML = tpl(configurations['index']);
+	renderApp('index');
 
 	const links = document.querySelectorAll('.pages__item');
 
-	for (const link of links) {
-		link.addEventListener('click', (evt) => {
-			const current = evt.target.classList[1];
-
-			const template = document.createElement('template');
-			template.innerHTML = configurations[current];
-			document.getElementById('app').innerHTML = '';
-			document.getElementById('app').appendChild(template.content)
-		})
+	if (links) {
+		links.forEach(link => link.addEventListener('click', evt => renderApp(evt.target.classList[1])));
 	}
 };
